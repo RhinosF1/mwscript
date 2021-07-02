e7bdec7ec4637265364089eac62e64a3e1142824
RhinosF1 <rhinosf1@gmail.com>
2021-07-02 22:46:29 +0100

    update

--- mwscript.py
+++ mwscript.py
@@ -1,30 +1,31 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
 import os
+import sys
+MWPATH = 'srv/mediawiki/w'
 if len(sys.argv) < 3:
-    raise Exception("Not Enough Parameters")
+    raise Exception('Not Enough Parameters')
 script = sys.argv[1]
 if len(script.split('/')) == 1:
-    script = f'/srv/mediawiki/w/maintenance/{sys.argv[1]}'
+    script = f'{MWPATH}/maintenance/{sys.argv[1]}'
 else:
     scriptsplit = script.split('/')
-    if scriptsplit[2] == "removePII.php":
+    if scriptsplit[2] == 'removePII.php':
         raise Exception("RemovePII can't be executed with mwscript")
-    script = script = f'/srv/mediawiki/w/{scriptsplit[0]}/{scriptsplit[1]}/maintenance/{scriptsplit[2]}'
+    script = f'{MWPATH}/{scriptsplit[0]}/{scriptsplit[1]}/maintenance/{scriptsplit[2]}'
 wiki = sys.argv[2]
-if wiki in ("all", "foreachwikiindblist"):
+if wiki in ('all', 'foreachwikiindblist'):
     command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json {script}'
-elif wiki in ("extension", "skin"):
-    extension = input("Type the ManageWiki name of the extension or skin: ")
-    generate = f'php /srv/mediawiki/w/extensions/MirahezeMagic/maintenance/generateExtensionDatabaseList.php --wiki=loginwiki --extension={extension}'
+elif wiki in ('extension', 'skin'):
+    extension = input('Type the ManageWiki name of the extension or skin: ')
+    generate = f'php {MWPATH}/extensions/MirahezeMagic/maintenance/generateExtensionDatabaseList.php --wiki=loginwiki --extension={extension}'  #  noqa: E501
     command = f'sudo -u www-data /usr/local/bin/foreachwikiindblist /home/{os.getlogin()}/{extension}.json {script}'
 else:
     command = f'sudo -u www-data php {script} --wiki={wiki}'
 if len(sys.argv) == 4:
     command = f'{command} {sys.argv[3]}'
 logcommand = f'/usr/local/bin/logsalmsg "{command}"'
-print("Will execute:")
+print('Will execute:')
 if 'generate' in locals():
     print(generate)
 print(command)
